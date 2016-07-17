c61fbb5090526325b226aaa9e9c71ffa9e37bb4b
sorooshansari <sorosh.ansari@gmaill.com>
2016-07-17 13:04:40 +0430

    ready for test by app

---
+++ IAG_API_Server/IAG_API_Server/Controllers/PlaceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IAG_API_Server.Models;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace IAG_API_Server.Controllers
+{
+    public class PlaceController : ApiController
+    {
+        dbManager dbManager = new dbManager();
+        // GET: api/Place
+        public IEnumerable<PlaceBasicInfo> Get()
+        {
+            var dt = dbManager.TableResultSP("AllPlaces");
+            List<PlaceBasicInfo> res = new List<PlaceBasicInfo>();
+            foreach (DataRow dr in dt.Rows)
+                res.Add(new PlaceBasicInfo() { PlaceId = (Guid)dr["Id"], UpdateNum = (int)dr["UpdateNum"] });
+            return res;
+        }
+        // POST: api/Place
+        public IEnumerable<PlaceFullInfo> Post(params Guid[] IDs)
+        {
+            if (IDs == null || IDs.Length > 0)
+                return null;
+            var table = new DataTable();
+            table.Columns.Add("Id", typeof(Guid));
+            foreach (var item in IDs)
+                table.Rows.Add(item);
+            var pList = new SqlParameter("@list", SqlDbType.Structured);
+            pList.TypeName = "dbo.IdList";
+            pList.Value = table;
+            var dt = dbManager.TableResultSP("GetPlaceWithDetails", pList);
+            List<PlaceFullInfo> res = new List<PlaceFullInfo>();
+            foreach (DataRow dr in dt.Rows)
+                res.Add(new PlaceFullInfo()
+                {
+                    Id = (Guid)dr["[OnP_Id]"],
+                    Name = (string)dr["[OnP_Name]"],
+                    imgUrl = (string)dr["[OnP_ImgUrl]"],
+                    desc = (string)dr["[OnP_Discription]"],
+                    c_x = (float)dr["[OnP_cordinate_X]"],
+                    C_y = (float)dr["[OnP_cordinate_Y]"],
+                    address = (string)dr["[OnP_Address]"],
+                    CityName = (string)dr["[Cit_Name]"],
+                    UpdateNumber = (int)dr["[OnP_UpdateNumber]"]
+                });
+            return res;
+        }
+
+        // PUT: api/Place/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Place/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- IAG_API_Server/IAG_API_Server/Models/ViewModels.cs
+++ IAG_API_Server/IAG_API_Server/Models/ViewModels.cs
@@ -8,4 +8,21 @@ namespace IAG_API_Server.Models
     public class ViewModels
     {
     }
+    public class PlaceBasicInfo
+    {
+        public Guid PlaceId { get; set; }
+        public int UpdateNum { get; set; }
+    }
+    public class PlaceFullInfo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string imgUrl { get; set; }
+        public string desc { get; set; }
+        public float c_x { get; set; }
+        public float C_y { get; set; }
+        public string address { get; set; }
+        public string CityName { get; set; }
+        public int UpdateNumber { get; set; }
+    }
 }
