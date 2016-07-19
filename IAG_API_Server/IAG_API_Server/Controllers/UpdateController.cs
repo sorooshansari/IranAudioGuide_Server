6d410cc3d235783eff9e4e52032779aa42488c5f
sorooshansari <sorosh.ansari@gmaill.com>
2016-07-19 21:07:58 +0430

    get updates by post

--- IAG_API_Server/IAG_API_Server/Controllers/UpdateController.cs
+++ IAG_API_Server/IAG_API_Server/Controllers/UpdateController.cs
@@ -12,38 +12,16 @@ namespace IAG_API_Server.Controllers
 {
     public class UpdateController : ApiController
     {
-        dbManager dbManager = new dbManager();
+        dbTools dbTools = new dbTools();
         // GET: api/Update
-        public GetAllVM Get()
+        public void Get([FromBody]string value)
         {
-            var dt = dbManager.MultiTableResultSP("GetAll");
-            var res = new GetAllVM()
-            {
-                Places = FillPlaceVM(dt[0]),
-                Audios = FillAudioVM(dt[1]),
-                Cities = FillCityVM(dt[2]),
-                UpdateNumber = GetNumFromdataTable(dt[3])
-            };
-            return res;
         }
-        // GET: api/Update/5
-        public GetAllVM Get(int LastUpdateNumber)
+        
+        // POST: api/Update/5
+        public GetAllVM Post(int LastUpdateNumber)
         {
-            var SP = new SqlParameter("@UpdateNumber", LastUpdateNumber);
-            var dt = dbManager.MultiTableResultSP("GetUpdates", SP);
-            var res = new GetAllVM()
-            {
-                Places = FillPlaceVM(dt[0]),
-                Audios = FillAudioVM(dt[1]),
-                Cities = FillCityVM(dt[2]),
-                UpdateNumber = GetNumFromdataTable(dt[3])
-            };
-            return res;
-        }
-
-        // POST: api/Update
-        public void Post([FromBody]string value)
-        {
+            return dbTools.GetUpdate(LastUpdateNumber);
         }
 
         // PUT: api/Update/5
@@ -54,58 +32,6 @@ namespace IAG_API_Server.Controllers
         // DELETE: api/Update/5
         public void Delete(int id)
         {
-        }
-        private int GetNumFromdataTable(DataTable dataTable)
-        {
-            int res = 0;
-            if (dataTable.Rows.Count > 0)
-                res = (dataTable.Rows[0]["LastUpdate"] == DBNull.Value) ? 0 : (int)dataTable.Rows[0]["LastUpdate"];
-            return res;
-        }
-
-        private List<CitiesFullInfno> FillCityVM(DataTable dataTable)
-        {
-            List<CitiesFullInfno> res = new List<CitiesFullInfno>();
-            foreach (DataRow dr in dataTable.Rows)
-                res.Add(new CitiesFullInfno()
-                {
-                    Id = (int)dr["Id"],
-                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString()
-                });
-            return res;
-        }
-
-        private List<AudioFullInfo> FillAudioVM(DataTable dataTable)
-        {
-            List<AudioFullInfo> res = new List<AudioFullInfo>();
-            foreach (DataRow dr in dataTable.Rows)
-                res.Add(new AudioFullInfo()
-                {
-                    PlaceId = (Guid)dr["PlaceId"],
-                    ID = (Guid)dr["Id"],
-                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString(),
-                    Desc = (dr["Descript"] == DBNull.Value) ? string.Empty : dr["Descript"].ToString(),
-                    Url = (dr["Url"] == DBNull.Value) ? string.Empty : dr["Url"].ToString()
-                });
-            return res;
-        }
-
-        private List<PlaceFullInfo> FillPlaceVM(DataTable dataTable)
-        {
-            List<PlaceFullInfo> res = new List<PlaceFullInfo>();
-            foreach (DataRow dr in dataTable.Rows)
-                res.Add(new PlaceFullInfo()
-                {
-                    Id = (Guid)dr["Id"],
-                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString(),
-                    ImgUrl = (dr["Url"] == DBNull.Value) ? string.Empty : dr["Url"].ToString(),
-                    Desc = (dr["Descript"] == DBNull.Value) ? string.Empty : dr["Descript"].ToString(),
-                    CX = (double)dr["X"],
-                    CY = (double)dr["Y"],
-                    Address = (dr["Adr"] == DBNull.Value) ? string.Empty : dr["Adr"].ToString(),
-                    CityId = (int)dr["CityId"]
-                });
-            return res;
         }
 
     }
---
+++ IAG_API_Server/IAG_API_Server/Models/dbTools.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace IAG_API_Server.Models
+{
+    public class dbTools
+    {
+        dbManager dbManager = new dbManager();
+        public GetAllVM GetUpdate(int LastUpdateNumber)
+        {
+            List<DataTable> dt;
+            if (LastUpdateNumber == 0)
+                dt = dbManager.MultiTableResultSP("GetAll");
+            else
+            {
+                var SP = new SqlParameter("@UpdateNumber", LastUpdateNumber);
+                dt = dbManager.MultiTableResultSP("GetUpdates", SP);
+            }
+            var res = new GetAllVM()
+            {
+                Places = FillPlaceVM(dt[0]),
+                Audios = FillAudioVM(dt[1]),
+                Cities = FillCityVM(dt[2]),
+                UpdateNumber = GetNumFromdataTable(dt[3])
+            };
+            return res;
+        }
+        private int GetNumFromdataTable(DataTable dataTable)
+        {
+            int res = 0;
+            if (dataTable.Rows.Count > 0)
+                res = (dataTable.Rows[0]["LastUpdate"] == DBNull.Value) ? 0 : (int)dataTable.Rows[0]["LastUpdate"];
+            return res;
+        }
+
+        private List<CitiesFullInfno> FillCityVM(DataTable dataTable)
+        {
+            List<CitiesFullInfno> res = new List<CitiesFullInfno>();
+            foreach (DataRow dr in dataTable.Rows)
+                res.Add(new CitiesFullInfno()
+                {
+                    Id = (int)dr["Id"],
+                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString()
+                });
+            return res;
+        }
+
+        private List<AudioFullInfo> FillAudioVM(DataTable dataTable)
+        {
+            List<AudioFullInfo> res = new List<AudioFullInfo>();
+            foreach (DataRow dr in dataTable.Rows)
+                res.Add(new AudioFullInfo()
+                {
+                    PlaceId = (Guid)dr["PlaceId"],
+                    ID = (Guid)dr["Id"],
+                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString(),
+                    Desc = (dr["Descript"] == DBNull.Value) ? string.Empty : dr["Descript"].ToString(),
+                    Url = (dr["Url"] == DBNull.Value) ? string.Empty : dr["Url"].ToString()
+                });
+            return res;
+        }
+
+        private List<PlaceFullInfo> FillPlaceVM(DataTable dataTable)
+        {
+            List<PlaceFullInfo> res = new List<PlaceFullInfo>();
+            foreach (DataRow dr in dataTable.Rows)
+                res.Add(new PlaceFullInfo()
+                {
+                    Id = (Guid)dr["Id"],
+                    Name = (dr["Name"] == DBNull.Value) ? string.Empty : dr["Name"].ToString(),
+                    ImgUrl = (dr["Url"] == DBNull.Value) ? string.Empty : dr["Url"].ToString(),
+                    Desc = (dr["Descript"] == DBNull.Value) ? string.Empty : dr["Descript"].ToString(),
+                    CX = (double)dr["X"],
+                    CY = (double)dr["Y"],
+                    Address = (dr["Adr"] == DBNull.Value) ? string.Empty : dr["Adr"].ToString(),
+                    CityId = (int)dr["CityId"]
+                });
+            return res;
+        }
+    }
+}
