cf581e1e8ca8dbc7defaaac6167d64f644cb39c3
soroosh ansari mehr <soroosh.ansari.mehr@gmail.com>
2017-02-11 08:56:04 +0330

    add profile user

--- IranAudioGuide_MainServer/IranAudioGuide_MainServer/Models/ViewModels.cs
+++ IranAudioGuide_MainServer/IranAudioGuide_MainServer/Models/ViewModels.cs
@@ -151,7 +151,23 @@ public class PaymentResult
         [Display(Name = "Package Cities")]
         public List<int> Cities { get; set; }
     }
-
+    public class IPData
+    {
+        public string status { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string region { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string timezone { get; set; }
+        public string isp { get; set; }
+        public string org { get; set; }
+        public string @as { get; set; }
+        public string query { get; set; }
+    }
     public class PackageVM
     {
         public int Index { get; internal set; }
--- IranAudioGuide_MainServer/IranAudioGuide_MainServer/Scripts/UserControllers.js
+++ IranAudioGuide_MainServer/IranAudioGuide_MainServer/Scripts/UserControllers.js
@@ -1,42 +1,15 @@
 ﻿//Developed by pourmand
-userApp.filter('propsFilter', function () {
-    return function (items, props) {
-        var out = [];
 
-        if (angular.isArray(items)) {
-            var keys = Object.keys(props);
-
-            items.forEach(function (item) {
-                var itemMatches = false;
-
-                for (var i = 0; i < keys.length; i++) {
-                    var prop = keys[i];
-                    var text = props[prop].toLowerCase();
-                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                        itemMatches = true;
-                        break;
-                    }
-                }
-
-                if (itemMatches) {
-                    out.push(item);
-                }
-            });
-        } else {
-            // Let the output be the input untouched
-            out = items;
-        }
-
-        return out;
-    };
-}).controller('userCtrl', ['$scope', 'userServices', '$timeout', 'notificService', function ($scope, userServices, $timeout, notific) {
+userApp.controller('userCtrl', ['$scope', 'userServices', '$timeout', 'notificService', function ($scope, userServices, $timeout, notific) {
     $scope.user = {
         isAutintication: false,
         username: "test"
     }
 
     $scope.profile = {
-        istest: true
+        istest: true,
+        packages: [],
+        packagesPurchased:[]
     };
     userServices.getUser().then(function (data) {
         $scope.user = data;
@@ -51,25 +24,29 @@ userApp.filter('propsFilter', function () {
 
     $scope.profile.isCompletedLoading = false;
     $scope.getPackagesPurchased = function (event) {
+        if ($scope.profile.packagesPurchased.length == 0) {
 
-        var calssName = $(event.target).find("i").attr('class');
-        var s = $(event.target).find("i").removeAttr('class').addClass("fa fa-spinner fa-spin");
+            var calssName = $(event.target).find("i").attr('class');
+            var s = $(event.target).find("i").removeAttr('class').addClass("fa fa-spinner fa-spin");
 
-        userServices.getPackagesPurchased().then(function (data) {
-            if (data.length == 0) {
-                $scope.profile.packages = [];
-                $scope.profile.city = "";
-                $scope.IsShowMessage = true;
-            }
-            else {
-                $scope.profile.packages = data;
-                $scope.profile.city = data[0].PackageCities[0];
-            }
-            $timeout(function () {
-                nav();
+            userServices.getPackagesPurchased().then(function (data) {
+                if (data.length == 0) {
+                    $scope.profile.packagesPurchased = [];
+                    $scope.profile.city = "";
+                    $scope.IsShowMessage = true;
+                }
+                else {
+                    $scope.profile.packagesPurchased = data;
+                    $scope.profile.city = data[0].PackageCities[0];
+                }
                 $(event.target).find("i").removeAttr('class').addClass(calssName);
-            }, 2000);
-        })
+
+                //$timeout(function () {
+                //    nav();
+                //    $(event.target).find("i").removeAttr('class').addClass(calssName);
+                //}, 2000);
+            })
+        }
 
     }
     $scope.deactivateMobile = function () {
@@ -82,16 +59,18 @@ userApp.filter('propsFilter', function () {
     }
     $scope.getPackages = function (event) {
 
-        var element = $(event.target).find("i");
-        var calssName = element.attr('class');
-        var s = element.removeAttr('class').addClass("fa fa-spinner fa-spin");
+        if ($scope.profile.packages.length == 0) {
+            var element = $(event.target).find("i");
+            var calssName = element.attr('class');
+            var s = element.removeAttr('class').addClass("fa fa-spinner fa-spin");
+            userServices.getPackages().then(function (data) {
+                $scope.profile.packages = data;
+                element.removeAttr('class').addClass(calssName);
+                $scope.profile.isCompletedLoading = true;
+                $(event.target).find("i").removeAttr('class').addClass(calssName);
+            })
+        }
 
-        userServices.getPackages().then(function (data) {
-            $scope.profile.packages = data;
-            element.removeAttr('class').addClass(calssName);
-            $scope.profile.isCompletedLoading = true;
-        })
-     
     }
     //$scope.profile.isCompletedLoading = true;
 
@@ -136,56 +115,202 @@ userApp.filter('propsFilter', function () {
             $(this).parent().addClass('active')
         });
     };
-}])
-.filter('propsFilter', function () {
-    return function (items, props) {
-        var out = [];
-
-        if (angular.isArray(items)) {
-            var keys = Object.keys(props);
-
-            items.forEach(function (item) {
-                var itemMatches = false;
-
-                for (var i = 0; i < keys.length; i++) {
-                    var prop = keys[i];
-                    var text = props[prop].toLowerCase();
-                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                        itemMatches = true;
-                        break;
-                    }
-                }
-
-                if (itemMatches) {
-                    out.push(item);
-                }
-            });
-        } else {
-            // Let the output be the input untouched
-            out = items;
-        }
-
-        return out;
-    };
-});
+}]);
 userApp.controller('PackagesCtrl', ['$scope', 'userServices', '$timeout', function ($scope, userServices, $timeout) {
-    if (typeof $scope.profile.packages == undefined)
-        return;
+    //if (typeof $scope.profile.packages == undefined)
+    //    return;
+    //var form = document.getElementById("PurchaseForm")
+    //    //,            select = document.getElementById("pt");
+
+    //form.addEventListener("submit", function (event) {
+    //    event.preventDefault();
+    //    if (select.value == 2) {
+    //        var element = document.createElement("INPUT");
+    //        element.setAttribute("type", "hidden");
+    //        element.setAttribute("name", "PackageId");
+    //        element.setAttribute("value", $scope.packagesId);
+    //        form.appendChild(element);
+    //    }
+    //    form.submit();
+    //});
+
+
+    $scope.buyPakages = function (pak) {
+        var form = document.getElementById("PurchaseForm");
+        var element = document.createElement("INPUT");
+        element.setAttribute("type", "hidden");
+        element.setAttribute("name", "PackageId");
+        element.setAttribute("value", pak.PackageId);
+        form.appendChild(element);
+        form.submit();
+    }
+
+
+    $scope.typeEachItemFoSelection = [
+        { type: 0, name: 'City', icon: "fa fa-bolt", className: "itemSelcted box-city" },
+        { type: 1, name: 'Place', icon: "fa fa-leaf", className: "itemSelcted box-place" }
+    ];
+    $scope.searhPakage = {
+        item: []
+    };
+
+
+
     $scope.$watch("profile.packages", function (newval, oldval) {
         if (typeof newval != undefined) {
             initPakage();
         }
     });
-    var initPakage = function () {
-        //console.log($scope.profile.packages);
-        $scope.pakageForSelected = [];
-        _.each($scope.profile.packages, function (item) {
-            //set city
-            //$scope.pakageForSelected.push({
 
-            //})
-            console.log("item:",item)
+    var intersection = function (searchItem) {
+
+        for (var i = 0; i < $scope.listPakages.length; i++) {
+            var pak = $scope.listPakages[i];
+            var findPak = false;
+            for (var inewp = 0; inewp < searchItem.packages.length  ; inewp++)//&& $scope.listPakages.length != 0
+            {
+                var newPak = searchItem.packages[inewp];
+                if (pak.PackageId == newPak.PackageId) {
+                    findPak = true;
+                    break;
+                }
+            }//end search 2
+            if (!findPak)
+                $scope.listPakages.pop(pak);
+        }
+
+        if ($scope.listPakages.length == 0)
+            $scope.listPakages = searchItem.packages;
+    }
+
+    var setPakages = function (searchItem) {
+
+        var isFindCity = false;
+        //چک می کنیم که این شهر قبلا انتخاب شده یا نه
+        for (var i = 0; i < arrayCity.length; i++) {
+            if (arrayCity[i].CityID == searchItem.CityID) {
+                isFindCity = true;
+                break;
+            }
+        }
+        if (arrayCity.length == 0 || !isFindCity) {
+            arrayCity.push({ CityID: searchItem.CityID, packages: searchItem.packages });
+            intersection(searchItem);
+
+        }
+    }
+    var arrayCity = [];
+    $scope.listPakages = [];
+
+    $scope.$watch("searhPakage.item", function (listSelected, oldval) {
+
+        //var tempList = angular.copy(listSelected);
+        //angular.copy(listSelected, tempList);
+        //var tlist = [];
+        //tlist = tlist.concat(listSelected);
+        if (typeof listSelected == undefined || listSelected.length == 0) {
+            arrayCity = [];
+            $scope.listPakages = angular.copy($scope.profile.packages);
+            return;
+        }
+
+        // delete city of search
+        if (listSelected.length < oldval.length) {
+            var findItem = false;
+            var searchItem = _.difference(listSelected, oldval);
+            //چک می کنیم که این شهر قبلا انتخاب شده یا نه
+            for (var i = 0; i < listSelected.length; i++) {
+                if (searchItem.CityID == listSelected[i].CityID) {
+                    findItem = true;
+                    break;
+                }
+            }
+            if (findItem)
+                return;
+            arrayCity = [];
+            $scope.listPakages = [];
+            for (var i = 0; i < listSelected.length; i++) {
+                var searchItem = angular.copy(listSelected[i]);
+                setPakages(searchItem);
+            }
+        } // end delete city of search
+
+        else {
+            //add search
+            var searchItem = angular.copy(listSelected[listSelected.length - 1]);
+            setPakages(searchItem);
+        }
+    });
+
+    //$scope.$watch("listPakages", function (newval) {
+    //    if (newval.length != 0 )
+    //        $timeout(function () {
+
+    //            $(".slider6").responsiveSlides({
+    //                auto: false,
+    //                pager: false,
+    //                nav: true,
+    //                speed: 500,
+    //                maxwidth: 800,
+    //                namespace: "centered-btns"
+    //            });
+    //        }, 5000)
+    //});
+
+    var initPakage = function () {
+
+        $scope.pakageForSelected = [];
+        $scope.selectId = 0;
+        $scope.pakageForSelected = [];
+        _.each($scope.profile.packages, function (pak) {
+            _.each(pak.PackageCities, function (city) {
+                var findItem = false;
+                for (var i = 0; i < $scope.pakageForSelected.length; i++) {
+                    var itemEntry = $scope.pakageForSelected[i];
+                    if (itemEntry.idItem == city.CityID) {
+                        itemEntry.packages.push(pak)
+                        findItem = true;
+                        break;
+                    }
+                }
+                if (!findItem)
+                    $scope.pakageForSelected.push({
+                        id: $scope.selectId++,
+                        idItem: city.CityID,
+                        // این گزینه برای این است که در جستجو  پکبج ها مشکلی پیش نیاد
+                        CityID: city.CityID,// 
+                        type: $scope.typeEachItemFoSelection[0].type,
+                        title: city.CityName,
+                        description: "There are " + city.Places.length + " locations for this city",
+                        packages: [pak]
+                    })
+                _.each(city.Places, function (plc) {
+                    var findItem = false;
+                    for (var i = 0; i < $scope.pakageForSelected.length; i++) {
+                        var itemEntry = $scope.pakageForSelected[i];
+                        if (itemEntry.idItem == plc.PlaceId) {
+                            itemEntry.packages.push(pak)
+                            findItem = true;
+                            break;
+                        }
+                    }
+                    if (!findItem)
+                        $scope.pakageForSelected.push({
+                            id: $scope.selectId++,
+                            idItem: plc.PlaceId,
+                            CityID: city.CityID,
+                            type: $scope.typeEachItemFoSelection[1].type,
+                            title: plc.PlaceName,
+                            description: "There is this place in " + city.CityName,
+                            packages: [pak]
+                        })
+                });//end each place
+            });//end each pack.PackageCities
+
         })//end each $scope.profile.package
+
+        $scope.listPakages = angular.copy($scope.profile.packages);
+
     }
     //_
     //$scope.profile.city = data[0].PackageCities[0];
@@ -195,36 +320,35 @@ userApp.controller('PackagesCtrl', ['$scope', 'userServices', '$timeout', functi
     //    return num * 3;
     //}); 
 
-    var typeEachItemFoSelection = ['City', 'Place'];
-    $scope.pakageForSelected = [{
-        id: 1,
-        idItem:0,
-        type: typeEachItemFoSelection[0],
-        title: "Siraz",
-        description: "There are 3 locations for this city",
-        iconClass: "fa fa-bolt",
-       
-    },{
-        id: 2,
-        idItem:0,
-        type: typeEachItemFoSelection[1],
-        title: "Jundishapur",
-        description: "There is this place in Shiraz",
-        iconClass:  "fa fa-leaf",
-       
-    }, {
-        id: 3,
-        idItem: 0,
-        type: typeEachItemFoSelection[1],
-        title: "hafezieh",
-        description: "There is this place in Shiraz",
-        iconClass: "fa fa-leaf",
+    //$scope.pakageForSelected = [{
+    //    id: 1,
+    //    idItem: 0,
+    //    type: typeEachItemFoSelection[0],
+    //    title: "Siraz",
+    //    description: "There are 3 locations for this city",
 
-    }]
+
+    //}, {
+    //    id: 2,
+    //    idItem: 0,
+    //    type: typeEachItemFoSelection[1],
+    //    title: "Jundishapur",
+    //    description: "There is this place in Shiraz",
+    //    // iconClass: ,
+
+    //}, {
+    //    id: 3,
+    //    idItem: 0,
+    //    type: typeEachItemFoSelection[1],
+    //    title: "hafezieh",
+    //    description: "There is this place in Shiraz",
+    //    iconClass: "fa fa-leaf",
+
+    //}]
     //$scope.productStatuce = $scope.productStatuse;
-    $scope.modelItem = {
-        StatusType: $scope.pakageForSelected[0]
-    };
+    //$scope.modelItem = {
+    //    StatusType: $scope.pakageForSelected[0]
+    //};
     //$scope.getProductStatus = function () {
     //    var deferred = $q.defer();
     //    deferred.resolve($scope.productStatuce);
@@ -239,4 +363,11 @@ userApp.controller('PackagesCtrl', ['$scope', 'userServices', '$timeout', functi
 userApp.controller('pakagePurchasedCtrl', ['$scope', 'userServices', '$timeout', function ($scope, userServices, $timeout) {
     console.log($scope.p);
 
+
+    $scope.$watch("profile.packagesPurchased", function (newval, oldval) {
+        if (typeof newval != undefined) {
+            $scope.packagesPurchased = angular.copy($scope.profile.packagesPurchased);
+
+        }
+    });
 }]);
