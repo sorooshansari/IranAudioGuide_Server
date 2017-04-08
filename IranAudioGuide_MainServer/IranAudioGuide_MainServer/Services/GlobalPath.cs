340d35b975764e83c7f89f4c8807f6956db1f801
Pourmand <soroosh.ansari.mehr@gmail.com>
2017-04-08 18:22:20 +0430

    Base Color Changed to #15437f Some Payment index Changes

--- IranAudioGuide_MainServer/IranAudioGuide_MainServer/Services/GlobalPath.cs
+++ IranAudioGuide_MainServer/IranAudioGuide_MainServer/Services/GlobalPath.cs
@@ -3,9 +3,9 @@ namespace IranAudioGuide_MainServer.Services
 {
     public static class GlobalPath
     {
-      public static readonly string ConnectionString = @"Data Source=DESKTOP-UG1254U;Initial Catalog=iranaudi_test1;Integrated Security=True";
+      //public static readonly string ConnectionString = @"Data Source=DESKTOP-UG1254U;Initial Catalog=iranaudi_test1;Integrated Security=True";
         public static readonly string ConnectionStringElmah = "Password = Zcvd14?3; Persist Security Info=True;User ID = iranaudi_Elmah; Initial Catalog = iranaudi_Elmah; Data Source = 185.55.224.3";
-    //  public static readonly string ConnectionString = "Password = 1Kr?g4e7; Persist Security Info=True;User ID = iranaud1_admin; Initial Catalog = iranaud1_db; Data Source = 164.138.23.164";
+        public static readonly string ConnectionString = "Password = 1Kr?g4e7; Persist Security Info=True;User ID = iranaud1_admin; Initial Catalog = iranaud1_db; Data Source = 164.138.23.164";
         public static readonly string hostFtp = "ftp://lnx1.morvahost.com"; // "ftp://iranaudioguide.com/test.iranaudioguide.com";
         public static readonly string host = "http://iranaudioguide.net";// "http://test.iranaudioguide.com";
         public static readonly string UsernameFtp = "admin@iranaudioguide.net";//"pourmand";
@@ -15,7 +15,7 @@ namespace IranAudioGuide_MainServer.Services
         //public static readonly string PathImageCity = "Files/Cities";
         //public static readonly string PathImagePlace = "Files/Places";
         //public static readonly string PathImageExtras = "Files/Places/Extras";
-       // public static readonly string PathImageTumbnail = "Files/Places/TumbnailImages";
+        //public static readonly string PathImageTumbnail = "Files/Places/TumbnailImages";
         public static readonly string FullPathStory = host + "/Files/Stories/";
         public static readonly string FullPathAudios = host + "/Files/Audios/";
         public static readonly string FullPathImageCity = host + "/Files/Cities/";
