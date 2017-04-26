81a5624bb8dc758361db57a23c2f41df1a1da06f
danialby <danialby@gmail.com>
2017-04-26 13:05:03 +0430

    redesign how we are

--- IranAudioGuide_MainServer/IranAudioGuide_MainServer/Services/GlobalPath.cs
+++ IranAudioGuide_MainServer/IranAudioGuide_MainServer/Services/GlobalPath.cs
@@ -3,7 +3,7 @@ namespace IranAudioGuide_MainServer.Services
 {
     public static class GlobalPath
     {
-        public static readonly string ConnectionString = @"Data Source=DESKTOP-UG1254U;Initial Catalog=iranaudi_test1;Integrated Security=True";
+        public static readonly string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
         public static readonly string ConnectionStringElmah = "Password = Zcvd14?3; Persist Security Info=True;User ID = iranaudi_Elmah; Initial Catalog = iranaudi_Elmah; Data Source = 185.55.224.3";
       // public static readonly string ConnectionString = "Password = 1Kr?g4e7; Persist Security Info=True;User ID = iranaud1_admin; Initial Catalog = iranaud1_db; Data Source = 164.138.23.164";
         public static readonly string hostFtp = "ftp://lnx1.morvahost.com"; // "ftp://iranaudioguide.com/test.iranaudioguide.com";
